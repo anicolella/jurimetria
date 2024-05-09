226bfa1dd8621157baa04f364ec5b88d50177fa2
anicolella <nicolella@usp.br>
2024-05-09 16:39:59 -0300

    regressao simples e suas interpretacoes

--- rascunho.R
+++ rascunho.R
@@ -6,3 +6,8 @@ p<-ggplot(final_fem_22, aes(y=feminic_tx)) +
 p+labs(title="Tx Feminicídio por 100 mil no Brasil 2022",
        x ="", y = "Tx Feminicídio")
 
+library(sm)
+sm.density.compare(final_fem_22$feminic_tx, final_fem_22$N_NE_CO, xlab="Taxa de Feminicídio", lwd=c(3, 3),col=c('darkgreen', 'darkblue'))
+title(main="Comparação da taxa de feminicídio entre regiôes")
+
+legend("topright", c("N NE CO", "S SD"), xpd = TRUE, horiz = FALSE , bty = "n",  col=c('darkgreen', 'darkblue'), lwd=c(4, 4), cex = 0.6)
